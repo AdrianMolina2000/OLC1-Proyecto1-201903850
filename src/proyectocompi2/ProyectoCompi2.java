e096f6c95298a1e13e0cb43c31bce472a4660697
Adrian Samuel Molina Cabrera <64050699+AdrianMolina2000@users.noreply.github.com>
2021-02-26 01:36:43 -0600

    Primer Commit

---
+++ src/proyectocompi2/ProyectoCompi2.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectocompi2;
+
+import java.io.FileInputStream;
+/**
+ *
+ * @author Adrian
+ */
+public class ProyectoCompi2 {
+    
+    public static void main(String[] args) {
+        interpretar("prueba.txt");
+    }
+    
+    private static void interpretar(String path) {
+        analizadores.Sintactico pars;
+        try {
+            pars=new analizadores.Sintactico(new analizadores.Lexico(new FileInputStream(path)));
+            pars.parse();        
+        } catch (Exception ex) {
+            System.out.println("Error fatal en compilación de entrada.");
+            System.out.println("Causa: "+ex.getCause());
+        } 
+    }
+    
+}
